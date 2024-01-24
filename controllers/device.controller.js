cf040df0646c0f5c6ce35d95ede1742988da46d4
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-25 00:42:56 +0200

    Add device controller with CRUD operations

---
+++ controllers/device.controller.js
@@ -0,0 +1,108 @@
+const { Device } = require("../models");
+const createDevice = async (req, res) => {
+  try {
+    const device = await Device.create(req.body);
+    res.status(201).json(device);
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.status(400).json({ message: "Device already exists" });
+    } else {
+      res.status(500).json({ message: "Error Creating Device" });
+    }
+  }
+};
+const getAllDevice = async (req, res) => {
+  try {
+    const devices = await Device.findAll();
+    res.status(200).json(devices);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving devices" });
+  }
+};
+const getDeviceById = async (req, res) => {
+  const deviceId = req.params.id;
+
+  try {
+    const device = await Device.findByPk(deviceId);
+
+    if (!device) {
+      return res
+        .status(404)
+        .json({ message: `Device with ID ${deviceId} not found` });
+    }
+
+    res.json(device);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving device" });
+  }
+};
+const updateDevice = async (req, res) => {
+  const deviceId = req.params.id;
+
+  try {
+    const device = await Device.findByPk(deviceId);
+
+    if (!device) {
+      return res
+        .status(404)
+        .json({ message: `Device with ID ${deviceId} not found` });
+    }
+
+    await device.update(req.body);
+
+    res.json(device);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating device" });
+  }
+};
+const deleteDevice = async (req, res) => {
+  const deviceId = req.params.id;
+
+  try {
+    const device = await Device.findByPk(deviceId);
+
+    if (!device) {
+      return res
+        .status(404)
+        .json({ message: `Device with ID ${deviceId} not found` });
+    }
+
+    await device.destroy();
+
+    res.json({ message: `Device with ID ${deviceId} deleted` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting device" });
+  }
+};
+//toggle status
+const toggleDeviceStatus = async (req, res) => {
+  const deviceId = req.params.id;
+
+  try {
+    const device = await Device.findByPk(deviceId);
+    if (!device) {
+      return res
+        .status(404)
+        .json({ message: `Device with ID ${deviceId} not found` });
+    }
+
+    await device.update(req.body);
+
+    res.json(device);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating device" });
+  }
+};
+module.exports = {
+  createDevice,
+  getAllDevice,
+  getDeviceById,
+  updateDevice,
+  deleteDevice,
+  toggleDeviceStatus,
+};

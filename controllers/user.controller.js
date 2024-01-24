38b6c8ffa1b0a168d2fb9402fed31a611fc75fc3
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-24 22:56:09 +0200

    Add createUser function to user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,4 +11,46 @@ const getAllUsers = async (req, res) => {
     res.status(500).send({ error: error });
   }
 };
+const createUser = async (req, res) => {
+  try {
+    const { UserId, email, tel, password, fullName, gender } = req.body;
+    if (!UserId || !email || !tel || !fullName || !gender) {
+      return res.status(400).json({
+        error: "All Fields Are Required",
+      });
+    }
+    const userWithId = await Users.findOne({ where: { UserId } });
+    const userWithEmail = await Users.findOne({ where: { email } });
+    const userWithPhone = await Users.findOne({ where: { tel } });
+    if (userWithId || userWithEmail || userWithPhone) {
+      return res.status(400).json({
+        error: "User already exists with this id, email, or phone",
+      });
+    }
+
+    const hashedPassword = password
+      ? await bcrypt.hash(password, 10)
+      : await bcrypt.hash(process.env.DefaultStudentPassword, 10);
+
+    const result = await Users.create({
+      UserId,
+      email,
+      tel,
+      fullName,
+      gender,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Data inserted successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: error,
+      message: "Error inserting data",
+    });
+  }
+};
 export { getAllUsers };

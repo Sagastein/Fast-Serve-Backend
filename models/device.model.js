04babf82be5ccfcecbb23f6e3cbb1e7322fbe802
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-24 23:18:41 +0200

    Add server.js file with API endpoints

---
+++ models/device.model.js
@@ -0,0 +1,53 @@
+const moment = require("moment");
+module.exports = (sequelize, Datatype) => {
+  const Device = sequelize.define("Device", {
+    id: {
+      type: Datatype.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Datatype.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Datatype.INTEGER,
+      allowNull: false,
+    },
+    location: {
+      type: Datatype.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Datatype.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["active", "blocked"]],
+      },
+      defaultValue: "active",
+    },
+
+    createdAt: {
+      type: Datatype.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      get() {
+        return moment(this.getDataValue("createdAt")).format(
+          "DD-MM-YYYY HH:mm:ss a"
+        );
+      },
+    },
+    updatedAt: {
+      type: Datatype.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      get() {
+        return moment(this.getDataValue("updatedAt")).format(
+          "Do,MMM YY, h:mm a"
+        );
+      },
+    },
+  });
+
+  return Device;
+};

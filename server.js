50ce8aeaac4164639832cef0f4b70bfce3ea0bd3
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-26 00:03:40 +0200

    Add routes for authentication and user management

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const db = require("./models");
+const {
+  AccountRouter,
+  DeviceRouter,
+  TransactionRouter,
+  UserRouter,
+  AuthRouter,
+} = require("./routes");
+
 var corsOptions = {
   origin: "http://localhost:5173",
 };
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/api/auth", AuthRouter);
+// app.use("/api/account", AccountRouter);
+app.use("/api/device", DeviceRouter);
+app.use("/api/transaction", TransactionRouter);
+app.use("/api/users", UserRouter);
 const port = 8080;
 const cardIds = [];
 app.get("/get/:id", (req, res) => {
@@ -24,7 +37,7 @@ app.get("/api/ca", (req, res) => {
   const cardId = cardIds.shift(); // Get the oldest card ID and remove it
   return res.status(200).json(cardId);
 });
-db.sequelize.sync({ alert: true }).then(() => {
+db.sequelize.sync({ alter: true }).then(() => {
   console.log("Database is connected");
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

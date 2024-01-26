36345a85d252aa2cf800ec1f0dc33b1a275d2d49
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-26 18:55:02 +0200

    Update account.route.js with new routes

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,8 +4,12 @@ const {
   createAccount,
   getAccountById,
   getAllACount,
+  getProfile,
+  toggleAccount,
 } = require("../controllers/account.controller");
-router.post("/create", createAccount);
+router.post("/", createAccount);
+router.get("/profile/:id", getProfile);
 router.get("/get/:id", getAccountById);
+router.patch("/toggle/:id", toggleAccount); 
 router.get("/", getAllACount);
 module.exports = router;
